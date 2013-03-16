2e7baffcf837a2c4e643095aab61b25f080065f5
Marco don Kaulea <DonMarco42@gmail.com>
2013-03-16 01:02:35 +0100

    Create QLineEdits to enter names

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -5,13 +5,25 @@
 
 MainWindow::MainWindow ()
 {
-    AddPlayerDialog dialog;
-    printf ("%d\n", dialog.exec ());
-    printf ("%d\n", dialog.getPlayerCount ());
+    //AddPlayerDialog dialog;
+    //printf ("%d\n", dialog.exec ());
+    //printf ("%d\n", dialog.getPlayerCount ());
+    setupPlayers ();
 }
 
 void
 MainWindow::setupPlayers ()
 {
+    AddPlayerDialog dialog;
+    QVBoxLayout *mainLayout = new QVBoxLayout;
 
+    if (dialog.exec() == 1)
+    {
+        int playerCount = dialog.getPlayerCount ();
+        for (int i = 0; i < playerCount; i++)
+        {
+           mainLayout->addWidget(new QLineEdit);
+        }
+    }
+    setLayout (mainLayout);
 }
